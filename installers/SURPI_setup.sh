af6c8a830cb55444e82d973946c74548bef89c0f
Jason Chang <crazycashier@icoud.com>
2016-06-14 09:57:01 +0800

    update setup script for biolinux8
    fix some compiling and sourcecode url errors for biolinux8
    add SRA Toolkit support

--- installers/SURPI_setup.sh
+++ installers/SURPI_setup.sh
@@ -41,7 +41,7 @@ CWD=$(pwd)
 
 # Install packages necessary for the SURPI pipeline.
 sudo -E apt-get update -y
-sudo -E apt-get install -y make csh htop python-dev gcc unzip g++ g++-4.6 cpanminus ghostscript blast2 python-matplotlib git pigz parallel ncbi-blast+
+sudo -E apt-get install -y qt-sdk make csh htop python-dev gcc unzip g++ g++-4.6 cpanminus ghostscript blast2 python-matplotlib git pigz parallel ncbi-blast+
 sudo -E apt-get upgrade -y
 
 #
@@ -55,9 +55,9 @@ wget http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip
 sudo mkdir /usr/local/ec2
 sudo unzip ec2-api-tools.zip -d /usr/local/ec2
 
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre" >> ~/.bashrc
-echo "export EC2_HOME=/usr/local/ec2/ec2-api-tools-1.6.13.0/" >> ~/.bashrc
-echo "PATH=\$PATH:$EC2_HOME/bin" >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre" >> ~/.zshrc
+echo "export EC2_HOME=/usr/local/ec2/ec2-api-tools-1.7.5.1" >> ~/.zshrc
+echo "PATH=\$PATH:$EC2_HOME/bin" >> ~/.zshrc
 
 #
 ##
@@ -80,11 +80,6 @@ sudo cpanm Net::OAuth
 ##
 #
 
-#Install via git clone
-# cd $bin_folder
-# sudo git clone https://github.com/chiulab/surpi.git
-# cd $CWD
-
 #Install specific version
 version="surpi-1.0.18"
 wget "https://github.com/chiulab/surpi/releases/download/v1.0.18/$version.tar.gz"
@@ -92,8 +87,7 @@ tar xvfz $version.tar.gz
 sudo mv $version "$bin_folder/"
 sudo ln -s "$bin_folder/$version" "$bin_folder/surpi"
 
-echo "PATH=\$PATH:$bin_folder" >> ~/.bashrc
-echo "PATH=\$PATH:$bin_folder/surpi" >> ~/.bashrc
+echo "PATH=\$PATH:$bin_folder/surpi" >> ~/.zshrc
 
 #
 ##
@@ -101,9 +95,9 @@ echo "PATH=\$PATH:$bin_folder/surpi" >> ~/.bashrc
 ##
 #
 #Works in Ubuntu 14.10
-curl -O "http://genometools.org/pub/genometools-1.5.4.tar.gz"
-tar xvfz genometools-1.5.4.tar.gz
-cd genometools-1.5.4
+curl -O "http://genometools.org/pub/genometools-1.5.7.tar.gz"
+tar xvfz genometools-1.5.7.tar.gz
+cd genometools-1.5.7
 make 64bit=yes curses=no cairo=no
 sudo make "prefix=$install_folder" 64bit=yes curses=no cairo=no install
 cd "$CWD"
@@ -113,10 +107,9 @@ cd "$CWD"
 ### install seqtk
 ##
 #
-# 6/4/14 - discovered that current version of seqtk (1.0-r57) is buggy. We should install 1.0-r31
-curl "https://codeload.github.com/lh3/seqtk/zip/1.0" > seqtk.zip
-unzip seqtk.zip
-cd seqtk-1.0
+wget "https://github.com/lh3/seqtk/archive/v1.2.tar.gz"
+tar xvfz v1.2.tar.gz
+cd seqtk-1.2
 make
 sudo mv seqtk "$bin_folder/"
 cd $CWD
@@ -153,9 +146,10 @@ cd $CWD
 ### install cutadapt
 ##
 #
-curl -O "https://cutadapt.googlecode.com/files/cutadapt-1.2.1.tar.gz"
-tar xvfz cutadapt-1.2.1.tar.gz
-cd cutadapt-1.2.1
+version="1.9.1"
+wget --no-check-certificate https://github.com/marcelm/cutadapt/archive/v$version.tar.gz -O cutadapt-$version.tar.gz
+tar xvfz cutadapt-$version.tar.gz
+cd cutadapt-$version
 python setup.py build
 sudo python setup.py install
 cd $CWD
@@ -165,10 +159,10 @@ cd $CWD
 ### install prinseq-lite.pl
 ##
 #
-
-curl -O "http://iweb.dl.sourceforge.net/project/prinseq/standalone/prinseq-lite-0.20.3.tar.gz"
-tar xvfz prinseq-lite-0.20.3.tar.gz
-sudo cp prinseq-lite-0.20.3/prinseq-lite.pl "$bin_folder/"
+version="0.20.4"
+wget http://nbtelecom.dl.sourceforge.net/project/prinseq/standalone/prinseq-lite-$version.tar.gz
+tar xvfz prinseq-lite-$version.tar.gz
+sudo cp prinseq-lite-$version/prinseq-lite.pl "$bin_folder/"
 sudo chmod +x "$bin_folder/prinseq-lite.pl"
 
 #
@@ -180,6 +174,7 @@ sudo chmod +x "$bin_folder/prinseq-lite.pl"
 sudo gcc $bin_folder/surpi/source/dropcache.c -o dropcache
 sudo mv dropcache "$bin_folder/"
 sudo chown root "$bin_folder/dropcache"
+# let the none root user can execute this command
 sudo chmod u+s "$bin_folder/dropcache"
 
 #
@@ -187,10 +182,10 @@ sudo chmod u+s "$bin_folder/dropcache"
 ### install SNAP
 ##
 #
-
-curl -O "http://snap.cs.berkeley.edu/downloads/snap-0.15.4-linux.tar.gz"
-tar xvfz snap-0.15.4-linux.tar.gz
-sudo cp snap-0.15.4-linux/snap "$bin_folder/"
+wget --no-check-certificate https://github.com/amplab/snap/releases/download/v1.0beta.18/snap-aligner
+sudo mv snap-aligner $bin_folder
+sudo chmod +x $bin_folder/snap-aligner
+sudo chown root:root $bin_folder/snap-aligner
 
 #
 ##
@@ -198,11 +193,11 @@ sudo cp snap-0.15.4-linux/snap "$bin_folder/"
 ##
 #
 
-curl "http://omics.informatics.indiana.edu/mg/get.php?justdoit=yes&software=rapsearch2.12_64bits.tar.gz" > rapsearch2.12_64bits.tar.gz
-tar xvfz rapsearch2.12_64bits.tar.gz
-cd RAPSearch2.12_64bits
+wget --no-check-certificate "https://github.com/zhaoyanswill/RAPSearch2/archive/master.zip" -O RAPSearch2-master.zip
+unzip RAPSearch2-master.zip
+cd RAPSearch2-master
 ./install
-sudo cp bin/* "$bin_folder/"
+sudo cp bin/* $bin_folder/
 cd $CWD
 
 #
@@ -210,58 +205,63 @@ cd $CWD
 ### install fastQValidator from sourcecode
 ##
 #
-# http://genome.sph.umich.edu/wiki/FastQValidator
+# http://genome.sph.umich.edu/wiki/fastQValidator
 
-curl -O "http://genome.sph.umich.edu/w/images/2/20/FastQValidatorLibStatGen.0.1.1a.tgz"
-tar xvf FastQValidatorLibStatGen.0.1.1a.tgz
-cd fastQValidator_0.1.1a
+version="0.1.1"
+wget --no-check-certificate https://github.com/statgen/fastQValidator/archive/v$version.tar.gz -O fastQValidator-$version.tar.gz
+tar xvzf fastQValidator-$version.tar.gz
+# download the libStatGen package
+git clone https://github.com/statgen/libStatGen.git
+cd fastQValidator-$version
 make all
-sudo cp fastQValidator/bin/fastQValidator "$bin_folder/"
+sudo cp bin/fastQValidator "$bin_folder/"
 cd $CWD
 
 
-#
-##
-### install AbySS 1.5.2
-##
-#
-# http://www.bcgsc.ca/platform/bioinfo/software/abyss
-
-#Download ABySS
-wget "https://github.com/bcgsc/abyss/releases/download/1.5.2/abyss-1.5.2.tar.gz"
-tar xvfz abyss-1.5.2.tar.gz
-
-#Set up Boost Dependency
-cd abyss-1.5.2
-wget "http://softlayer-dal.dl.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz"
-tar xvfz boost_1_57_0.tar.gz
-ln -s boost_1_57_0/boost boost
-
-#Install packaged dependencies
-#sudo apt-get install -y openmpi-bin sparsehash libopenmpi-dev # Ubuntu 12.04
-sudo apt-get install -y openmpi-bin libsparsehash-dev libopenmpi-dev # Ubuntu 14.10
-
-# Configure ABySS
-./configure --with-mpi=/usr/lib/openmpi CPPFLAGS=-I/usr/include/google
-make AM_CXXFLAGS=-Wall
-sudo make install
+# Installation notes for blat version 34
+# http://www.vcru.wisc.edu/simonlab/bioinformatics/programs/install/blat.htm
+ver="34"
+wget -N http://users.soe.ucsc.edu/~kent/src/blatSrc$ver.zip -O blatSrc-$ver.zip
+unzip blatSrc-$ver.zip
+cd blatSrc
+mkdir -p ~/bin/x86_64
+sed -i 's/HG_WARN_ERR = -DJK_WARN -Wall -Werror/HG_WARN_ERR = -DJK_WARN -Wall/g' inc/common.mk
+make 'MACHTYPE = x86_64'
+cp -puv ~/bin/x86_64/* $bin_folder/
+# clean up
+rm ~/bin/x86_64 -rf
 cd $CWD
 
-#
-##
-### install Minimo
-##
-#
-
-sudo apt-get -y install mummer
-sudo cpanm DBI
-sudo cpanm Statistics::Descriptive
-sudo cpanm XML::Parser
-
-curl -O "http://iweb.dl.sourceforge.net/project/amos/amos/3.1.0/amos-3.1.0.tar.gz"
-tar xvfz amos-3.1.0.tar.gz
-cd amos-3.1.0
-./configure --prefix=$install_folder CXX='g++-4.6'
+# install Jellyfish
+# via http://www.vcru.wisc.edu/simonlab/bioinformatics/programs/install/jellyfish.htm
+ver="1.1.11"
+wget -N http://www.cbcb.umd.edu/software/jellyfish/jellyfish-$ver.tar.gz
+tar -zxvf jellyfish-$ver.tar.gz
+cd jellyfish-$ver
+./configure --prefix=$install_folder
 make
 sudo make install
 cd $CWD
+
+
+# install amos pipeline
+# http://www.vcru.wisc.edu/simonlab/bioinformatics/programs/install/amos.htm
+
+sudo apt-get install libboost-graph-dev
+sudo cpan XML::Parser Config::IniFiles Statistics::Descriptive DBI
+git clone git://git.code.sf.net/p/amos/code amos-code
+cd amos-code
+./bootstrap
+./configure --prefix=$install_folder
+make CXXFLAGS='-Wno-deprecated'
+sudo make install
+cd $CWD
+
+
+# Downloading and installing the SRA Toolkit
+# http://www.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=toolkit_doc&f=std#s-3
+wget "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz"
+tar xvzf sratoolkit.current-centos_linux64.tar.gz
+sudo mv sratoolkit.2.6.3-centos_linux64 /usr/local/
+sudo ln -s /usr/local/sratoolkit.2.6.3-centos_linux64 /usr/local/sratoolkit
+
